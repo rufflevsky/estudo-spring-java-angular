d0e89df50cf193f3aed603f56bb3556f16a15869
Rafael Munoz <rafael.muno@gmail.com>
2022-02-11 12:53:48 -0300

    Inclusão de evento/listener para montagem da header location

--- src/main/java/com/rafael/projetofinancapi/resource/CategoriaResource.java
+++ src/main/java/com/rafael/projetofinancapi/resource/CategoriaResource.java
@@ -6,18 +6,18 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import java.net.URI;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.rafael.projetofinancapi.event.RecursoCriadoEvent;
 import com.rafael.projetofinancapi.model.Categoria;
 import com.rafael.projetofinancapi.repository.CategoriaRepository;
 
@@ -27,6 +27,9 @@ public class CategoriaResource {
 		
 		@Autowired
 		private CategoriaRepository categoriaRepository;
+		
+		@Autowired
+		private ApplicationEventPublisher publisher;
 		
 		@GetMapping
 		public List<Categoria> listar(){
@@ -43,11 +46,9 @@ public class CategoriaResource {
 		public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 			Categoria categoriaSalva = categoriaRepository.save(categoria);
 			
-			URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-					.buildAndExpand(categoriaSalva.getNome()).toUri();
-			response.setHeader("Location", uri.toASCIIString());
+			publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
 			
-			return ResponseEntity.created(uri).body(categoriaSalva);
+			return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 			
 		}
 		
--- src/main/java/com/rafael/projetofinancapi/resource/PessoaResource.java
+++ src/main/java/com/rafael/projetofinancapi/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package com.rafael.projetofinancapi.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,15 +7,19 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.rafael.projetofinancapi.event.RecursoCriadoEvent;
 import com.rafael.projetofinancapi.model.Pessoa;
 import com.rafael.projetofinancapi.repository.PessoaRepository;
 
@@ -26,6 +29,9 @@ public class PessoaResource {
 	
 	@Autowired
 	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
 	public List<Pessoa> listar(){
@@ -41,13 +47,15 @@ public class PessoaResource {
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(pessoaSalva.getNome()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(pessoaSalva);
-		
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
 	}
 
 }
